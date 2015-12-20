9f8b779d271302a33a51f86c91753e66cda21a85
Sudeep162 <mohanty.sudeep162@gmail.com>
2015-12-20 22:02:39 +0530

    Added code

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main{
 public static void main(String args[])
 {
   System.out.println("Hello Git world");
-  System.out.println("Hello from outside");
-}
+  System.out.println("Hello  from  outside");
+  System.out.println("Hi");
+ }
 //Completed
